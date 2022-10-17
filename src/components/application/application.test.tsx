5b9b406d6bd5087b82042fe8031cb003ad8dc269
Bharath Mohan <bharath98mohan@gmail.com>
2022-10-17 14:48:28 +0530

    ep.22 completed

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -15,10 +15,21 @@ describe("Application", () => {
     });
     expect(sectionHeading).toBeInTheDocument();
 
+    const paragraphElement = screen.getByText("All fields are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
     const nameElement = screen.getByRole("textbox", {
       name: "Name",
     });
     expect(nameElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
 
     const bioElement = screen.getByRole("textbox", {
       name: "Bio",
@@ -30,6 +41,11 @@ describe("Application", () => {
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitbuttonElement = screen.getByRole("button");
     expect(submitbuttonElement).toBeInTheDocument();
