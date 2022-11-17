7f5cb66da2553df4377d62d045b7113dbcdf9fd4
Bharath Mohan <bharath98mohan@gmail.com>
2022-11-17 19:25:57 +0530

    ep.36 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Application } from "./components/application/application";
+import { Counter } from "./components/counter/counter";
 
 function App() {
   return (
     <div className="App">
       <Application />
+      <Counter />
     </div>
   );
 }
